b67ae266cd3c45bebb43c0b09289fa795585b167
Joaquin Sedoff <joacelrusito@gmail.com>
2025-03-27 16:46:53 -0300

    aproximacion

--- main.py
+++ main.py
@@ -11,34 +11,49 @@
 # red LED: ON == WiFi fail
 # green LED heartbeat: demonstrates scheduler is running.
 
-from rich import print
 from lib.mqtt_as import MQTTClient
 from lib.mqtt_local import config
 import uasyncio as asyncio
 from settings import SSID, password, BROKER
 
 
-import machine
+import machine, dht, ujson
 from machine import Pin
-import dht
+from collections import OrderedDict
+
+
+d = dht.DHT11(machine.Pin(13))
+
 
 #Parte de asyncio y mqtt
 
-#Funcion para imprimir un mensaje recibido mediante mqtt
 def sub_cb(topic, msg, retained):
     print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
-
-
 
 #funcion para mostrar el estado de la conexión wifi (primero se debe conectar)
 async def wifi_han(state):
     print('Wifi is ', 'up' if state else 'down')
     await asyncio.sleep(1)
 
-#suscripcion a los topicos de temperatura y humedad (mqtt)
+
+id = ""
+for b in machine.unique_id():
+  id += "{:02X}".format(b)
+print(id)
+
+#suscripcion a los topicos
 async def conn_han(client):
-    await client.subscribe('topico/temperatura', 1)
-    await client.subscribe('topico/humedad', 1)
+    await client.subscribe(id +'periodo', 1)
+    await client.subscribe(id +'rele', 1)
+    await client.subscribe(id +'setpoint', 1)
+    await client.subscribe(id +'destello', 1)
+    await client.subscribe(id +'modo', 1)
+#variables definidas por el usuario
+
+modo = 0
+setpoint = 20
+periodo = 10
+
 
 
 #Main definido como una funcion asyncio
@@ -46,36 +61,36 @@ async def conn_han(client):
 async def main(client):
     await client.connect()
     n = 0
-    setpoint = 0
-    modo = 0
-    rele = 0
     await asyncio.sleep(2)  # Give broker time
     while True:
         try:
             d.measure()
             try:
                 temperatura=d.temperature()
-                await client.publish('topico/temperatura', '{}'.format(temperatura), qos =  1) #Manda mensaje MQTT con la temperatura al topico temperatura
-            except OSError as e:
-                print("sin sensor temperatura")
-            try:
-                humedad=d.humidity()
-                await client.publish('topico/humedad', '{}'.format(humedad), qos = 1) #Manda mensaje MQTT con la humedad al topico humedad
+                try:
+                    humedad=d.humidity()
+  
+                except OSError as e:
+                    print("sin sensor temperatura")
             except OSError as e:
                 print("sin sensor humedad")
         except OSError as e:
             print("sin sensor")
-        
-        await client.publish('topico/setpoint', '{}'.format(setpoint), qos =  1)
 
-        if (temperatura > setpoint): rele = 1
-        await client.publish('topico/rele', '{}'.format(rele), qos =  1)
-
+        if ((d.temperature()>setpoint) and (modo == 1)): rele = 1
+        else: rele = 0
         
 
+        datos = ujson.dumps(OrderedDict([('temperatura',24),('humedad',53),
+                                        ('setpoint',setpoint),('periodo',periodo),('modo',modo)]))
+        
+        await client.publish(config['client_id'], datos, qos = 1)
 
-                 
-        await asyncio.sleep(20)  # Broker is slow
+
+        await asyncio.sleep(periodo)  # Broker is slow
+
+
+
 
 
 #Configuracion de MQTT
@@ -87,6 +102,7 @@ config['ssl'] = True
 config['ssid'] = SSID
 config['wifi_pw'] = password
 config['server'] = BROKER
+
 # Configurar cliente mqtt
 MQTTClient.DEBUG = True  # Optional
 client = MQTTClient(config)
@@ -95,14 +111,3 @@ try:
 finally:
     client.close()
     asyncio.new_event_loop()
-
-
-
-
-
-#Codigo para saber el ID del ESP32
-id = ""
-for b in machine.unique_id():
-  id += "{:02X}".format(b)
-print(id)
-
